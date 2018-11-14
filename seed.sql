eff633dd40b0ab15830832e08adf585b5a44447a
Edward Whelan <vroxing@gmail.com>
2018-11-14 11:58:52 -0500

    add seed data for answers table

--- seed.sql
+++ seed.sql
@@ -13,16 +13,57 @@ insert into users
   insert into health 
     (dailyscore, date, post, user_id)
       values
-    (1, '2018-12-25', 'Today was a good day', 1),
-    (1, '2018-12-25', 'Today was a good day', 2),
-    (1, '2018-12-25', 'Today was a good day', 3),
-    (1, '2018-12-25', 'Today was a good day', 4),
-    (1, '2018-12-25', 'Today was a good day', 5),
-    (1, '2018-12-25', 'Today was a good day', 6),
-    (1, '2018-12-25', 'Today was a good day', 7),
-    (1, '2018-12-25', 'Today was a good day', 1),
-    (1, '2018-12-25', 'Today was a good day', 2),
-    (1, '2018-12-25', 'Today was a good day', 3),
-    (1, '2018-12-25', 'Today was a good day', 4),
-    (1, '2018-12-25', 'Today was a good day', 5),
-    (1, '2018-12-25', 'Today was a good day', 6);
+    (5, '2018-12-25', 'Today was a good day', 1),
+    (8, '2018-12-25', 'Today was a bad day', 2),
+    (9, '2018-12-25', 'Today was an awful day', 3),
+    (2, '2018-12-25', 'Today was a random day', 4),
+    (3, '2018-12-25', 'Today was a ajshdkajshd day', 5),
+    (4, '2018-12-25', 'Today was a lsadjalsjdas day', 6),
+    (7, '2018-12-25', 'Today was a red day', 7),
+    (10, '2018-12-25', 'Today was a black day', 1),
+    (5, '2018-12-25', 'Today was a happy day', 2),
+    (4, '2018-12-25', 'Today was a sad day', 3),
+    (2, '2018-12-25', 'Today was a feeling day', 4),
+    (1, '2018-12-25', 'Today was a GREAT day', 5),
+    (1, '2018-12-25', 'Today was a day', 6);
+
+insert into questions
+  (questiontext)
+    values
+  (' How was your focus today?'),
+  ('How did your social interactions go today?'),
+  ('How did you feel about yourself today?'),
+  ('How did you feel physically?'),
+  ('How did you feel overall?');
+
+
+  insert into answers
+    (answers, date,  user_id, question_id)
+      values
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3),
+    (1, '2018-12-25', 1, 3);
+    
