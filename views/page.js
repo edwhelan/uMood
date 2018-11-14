d184aebb2388276dbb3641c09e254bf1d15e72d4
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-11-14 11:48:28 -0500

    adds page view

---
+++ views/page.js
@@ -0,0 +1,27 @@
+const helper = require(`./helper`);
+
+// Main Page View
+function page(content, isLoggedIn = false) {
+  return `
+    <!doctype html>
+    <html>
+      <head>
+        <meta charset="UTF=8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="stylesheets/index.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.css" rel="stylesheet">
+      </head>
+      <body>
+        <header>
+          ${
+    isLoggedIn ? helper.logoutButton() : helper.loginOrRegister()
+    }
+        </header>
+        ${content}
+      </body>
+    </html>
+    `
+}
+
+module.exports = page; 
