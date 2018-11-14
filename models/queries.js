d96f45bd830a6da6e47ef9be6948f45e575b26e9
Edward Whelan <vroxing@gmail.com>
2018-11-14 15:34:29 -0500

    add queries for add and retrieve

--- models/queries.js
+++ models/queries.js
@@ -7,11 +7,51 @@ function addAUser(displayname, emailaddress, password) {
   (displayname, emailaddress, password)
       values
   ($1, $2, $3)`, [displayname, emailaddress, password])
+    .then(results => {
+      console.log(results);
+    })
 }
 //create a health
-
+function addAHealth(dailyscore, date, notes, user_id) {
+  return db.any(`
+  insert into health 
+    (dailyscore, date, notes, user_id)
+      values
+    ($1, $2, $3, $4)`, [dailyscore, date, notes, user_id]
+  )
+}
 //make an answer
+function addAnAnswer(answer, date, user_id, question_id) {
+  return db.any(`insert into answers
+    (answer, date,  user_id, question_id)
+      values
+    ($1, $2, $3, $4)`, [answer, date, user_id, question_id]
+  )
+}
+
 //RETRIEVE
+//get questions
+function getQuestions() {
+  return db.any(`
+  select *
+  from
+  questions
+  `).then(console.log)
+}
+//get users by id
+function getUserByID(id) {
+  return db.one(`
+  select *
+  from 
+  users
+  where id=$1`, [id]
+  ).then(console.log)
+}
+//get display name
+
+//get health by date
+
+//get answer by date
 
 // UPDATE
 
@@ -19,4 +59,8 @@ function addAUser(displayname, emailaddress, password) {
 
 module.exports = {
   addAUser,
+  addAHealth,
+  addAnAnswer,
+  getQuestions,
+  getUserByID,
 }
